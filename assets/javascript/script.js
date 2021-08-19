f0006318daa0c9b68c67108106346468cd29a8af
spurgason <shanepurgason.98@gmail.com>
2021-08-19 18:25:57 -0400

    Password Generator Functioning
    -just need to make password only accept between 8 and 128 characters

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,61 @@
+var resultEl = document.getElementById('password')
+var generateEl = document.getElementById('generate')
+
+generateEl.addEventListener('click', () => {
+  var length = +confirmLength;
+  var hasLower = confirmLower; 
+  var hasUpper = confirmUpper;
+  var hasNumber = confirmNumber;
+  var hasSymbol = confirmSymbol;
+
+
+  resultEl.innerHTML = generatePassword(hasLower, hasUpper, hasNumber, hasSymbol, length)
+});
+
+// generate password function
+function generatePassword(lower, upper, number, symbol, length) {
+  var generatePassword = '';
+
+  var typesCount = lower + upper + number + symbol;
+
+  // console.log('typesCount', typesCount);
+
+  var typesArr = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+  // console.log('typesArr:', typesArr);
+
+  if (typesCount === 0) {
+    return '';
+  }
+
+  for(var i = 0; i < length; i += typesCount) {
+    typesArr.forEach(type => {
+      var funcName = Object.keys(type)[0];
+
+      // console.log('funcName', funcName);
+
+      generatePassword += randomFunc[funcName]();
+    });
+  }
+
+  return generatePassword;
+}
+// checking what they want in password
+var confirmLength = window.prompt('How many character would you like?')
+var confirmLower = window.confirm('Would you like lowercase letters?')
+var confirmUpper = window.confirm('Would you like uppercase letters?')
+var confirmNumber = window.confirm('Would you like numbers?')
+var confirmSymbol = window.confirm('Would you like symbols?')
+
+
+var randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+};
+
+
 // gets random lowercase letter
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -6,15 +64,15 @@ function getRandomLower() {
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-
+// gets random number
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-
-
+// gets random symbol
 function getRandomSymbol() {
-  const symbols = '!@#$%^&*()[]{}=<>/,.';
-  return symbols[0]
+  var symbols = '!@#$%^&*()[]{}=<>/,.';
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
 
-console.log(getRandomSymbol())
+
+
